46942cb90d77a1b51c627b603238c73eb24e4128
pdol <pdolinar@students.42wolfsburg.de>
2023-09-21 00:28:40 +0200

    segfault fix

--- dec_to_bin.c
+++ dec_to_bin.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
-#define LOG		printf
+#define LOG			printf
 #define BIT_SIZE	32
 
 // reset values
-void	reset_values(char **bin) {
+void	reset_values(char bin[BIT_SIZE]) {
 
 	for (int i = 0; i < BIT_SIZE; ++i)
-		bin[0][i] = '0';
-	bin[0][BIT_SIZE]='\0';
+		bin[i] = '0';
+	bin[BIT_SIZE]='\0';
 }
 
-void	convert(int x, char **bin) {
+void	convert(char bin[BIT_SIZE + 1], int x) {
 
 	reset_values(bin);
 
@@ -18,19 +18,19 @@ void	convert(int x, char **bin) {
 	for (; x >> i && i < BIT_SIZE; ++i) {
 		pos = x >> i;
 		if (pos & 0x01)
-			bin[0][BIT_SIZE - 1 - i] = '1';
+			bin[BIT_SIZE - 1 - i] = '1';
 #ifndef DEBUG
 		LOG("[ %d ] pos: %d\n",i,pos);
 #endif
 	}
 }
 
-void	run_tests(char **arr, int num_arr[], int size) {
+void	run_tests(char arr[][BIT_SIZE + 1], int num_arr[], int size) {
 
 	LOG("\n");	
 	for (int i = 0; i < size; ++i) {
-		convert(num_arr[i], &arr[i]);
-		LOG("arr:[%d] %-3d : %s\n", i,num_arr[i], (char *)&arr[i]);
+		convert(arr[i], num_arr[i]);
+		LOG("arr:[%d] %-4d : %s\n", i,num_arr[i], (char *)&arr[i]);
 	}
 }
 
@@ -43,22 +43,22 @@ int main() {
 	char	a[7][BIT_SIZE + 1] = {0};
 
 	// TEST
-//	int i = 1, j = 0;
-//	while (i < 128) {
-//		i *= 2;
-//		convert(i,(char **)&a[j]);
-//#ifndef DEBUG
-//		LOG("arr:[%-3d] binary value : %s\n", i,(char *)a[j]);
-//#endif
-//		j += 1;
-//	}
+	int i = 1, j = 0;
+	while (i < 128) {
+		i *= 2;
+		convert(a[j], i);
+#ifndef DEBUG
+		LOG("arr:[%-3d] binary value : %s\n", i,(char *)a[j]);
+#endif
+		j += 1;
+	}
 
 	// REAL TESTS
 	LOG("\n------   new tests  ------\n");
 	int size = sizeof(a) / sizeof(a[0]);
-	run_tests((char **)a, m, size);
-	run_tests((char **)a, n, size);
-	run_tests((char **)a, o, size);
+	run_tests(a, m, size);
+	run_tests(a, n, size);
+	run_tests(a, o, size);
 
 	return 0;
 }
